a89e02e910d4dd52e23981d02f771352e496a2ed
Ricardoagristamp <ricardo.agristamp@gmail.com>
2025-12-30 16:26:23 -0300

    Update: Dagster schedule to run every 1 minute and add volume mount for development

--- src/dagster/repository.py
+++ src/dagster/repository.py
@@ -1,6 +1,8 @@
 import os
 from dagster import Definitions, asset, DailyPartitionsDefinition, AssetExecutionContext
 from src.etl.etl import run as run_etl
+from .schedules import etl_schedule
+from .jobs import etl_job
 
 daily = DailyPartitionsDefinition(start_date="2025-12-01")
 
@@ -10,4 +12,8 @@ def etl_diario(context: AssetExecutionContext):
     run_etl(day, os.getenv("API_BASE_URL", "http://api:8000"))
     return day
 
-defs = Definitions(assets=[etl_diario])
+defs = Definitions(
+    assets=[etl_diario],
+    schedules=[etl_schedule],
+    jobs=[etl_job]
+)
--- src/dagster/schedules.py
+++ src/dagster/schedules.py
@@ -1,4 +1,4 @@
 from dagster import ScheduleDefinition
 from .jobs import etl_job
 
-etl_schedule = ScheduleDefinition(job=etl_job, cron_schedule="0 2 * * *")
+etl_schedule = ScheduleDefinition(job=etl_job, cron_schedule="*/1 * * * *")
