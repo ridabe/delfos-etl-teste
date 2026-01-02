8dc1fe5deafc223ddaba73b3804f7fa912658739
Ricardoagristamp <ricardo.agristamp@gmail.com>
2026-01-02 12:05:04 -0300

    chore: restaura agendamento diario padrao no dagster

--- src/dagster/schedules.py
+++ src/dagster/schedules.py
@@ -1,6 +1,7 @@
 from dagster import build_schedule_from_partitioned_job
 from .jobs import etl_job
 
+# Agenda a execução diária (padrão 00:00 UTC) baseada na partição do Job
 etl_schedule = build_schedule_from_partitioned_job(
     job=etl_job,
 )
